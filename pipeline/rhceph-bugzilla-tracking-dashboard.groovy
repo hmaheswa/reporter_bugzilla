098c647404408a4bf2e110e62f07e076d68d8d53
Hemanth Maheswarla <hmaheswa@hmaheswa.remote.csb>
2022-12-25 23:30:01 +0530

    adding pipeline groovy files for jenkins scheduled jobs
    Signed-off-by: Hemanth Maheswarla <hmaheswa@hmaheswa.remote.csb>

---
+++ pipeline/rhceph-bugzilla-tracking-dashboard.groovy
@@ -0,0 +1,72 @@
+/*
+    Pipeline script for executing rhceph 6.1 dashboard reports
+*/
+// file details
+def bugzilla_api_file = "/ceph/action_report/bugzillarc"
+def google_api_file = "/ceph/action_report/google_api_secret.json"
+def dest_bugzilla_api_path = "/home/jenkins-build/.config/python-bugzilla"
+def dest_google_api_path = "/home/jenkins-build/.gapi"
+
+// Pipeline script entry point
+node("rhel-8-medium || ceph-qe-ci") {
+    stage('prepareNode') {
+        if (env.WORKSPACE) { sh script: "sudo rm -rf * .venv" }
+        checkout(
+            scm: [
+                $class: 'GitSCM',
+                branches: [[name: 'main']],
+                extensions: [[
+                    $class: 'CleanBeforeCheckout',
+                    deleteUntrackedNestedRepositories: true
+                ], [
+                    $class: 'WipeWorkspace'
+                ], [
+                    $class: 'CloneOption',
+                    depth: 1,
+                    noTags: true,
+                    shallow: true,
+                    timeout: 10,
+                    reference: ''
+                ]],
+                userRemoteConfigs: [[
+                    url: 'https://github.com/hmaheswa/reporter_bugzilla.git'
+                ]]
+            ],
+            changelog: false,
+            poll: false
+        )
+    }
+
+stage("executeWorkflow") {
+    sh (script: '''
+    if [ ! -d '/ceph' ]; then
+    sudo mkdir -p /ceph
+    sudo mount -t nfs -o sec=sys,nfsvers=4.1 reesi004.ceph.redhat.com:/ /ceph
+    fi
+    ''')
+    println 'Mounted archive volume'
+
+    println "Install packages"
+    sh (script: "rm -rf .venv")
+    sh (script: "python3 -m venv .venv")
+    sh (script: ".venv/bin/python3 -m pip install --upgrade pip")
+    sh (script: ".venv/bin/python3 -m pip install -r requirements.txt")
+
+    sh (script : "sudo yum install postfix -y")
+    sh (script : "sudo systemctl enable postfix")
+    sh (script : "sudo systemctl start postfix")
+    sh (script : "sudo systemctl status postfix")
+
+    println "Adding API files"
+    sh (script : "mkdir -p ${dest_bugzilla_api_path}")
+    sh (script : "cp ${bugzilla_api_file} ${dest_bugzilla_api_path}")
+
+    sh (script : "mkdir -p ${dest_google_api_path}")
+    sh (script : "cp ${google_api_file} ${dest_google_api_path}")
+
+    println "Generating Action items"
+    sh (script: '.venv/bin/python3 tracking_dashboard.py "RHCS 6.1 QE - Quality Dashboard"')
+    
+    }
+}
+
